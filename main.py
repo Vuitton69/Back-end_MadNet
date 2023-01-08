ec6caa1089070b118f9ed922c018cb5da0711a53
Одинцов Дмитрий <dmodv@vk.com>
2023-01-08 21:23:28 +0300

    release

--- main.py
+++ main.py
@@ -16,14 +16,14 @@ def home():
     return 'You are at home page.'
 
 
-@app.route("/get", methods=['POST'])  # создание нового ивента
-def gettok():
+@app.route("/kerc1syuif&x^9!x*fl9kh@8vw05u4wlsf22ch9r", methods=['POST'])
+def get_tok():
     if request.method == "POST":
-        key = request.form['pubkey']
+        key = request.form['8aij&wvq0!1ow^5&x-mdl4sny!gniqxqa-yg*tfy']
         db = DB()
         for files in os.listdir('config'):
             try:
-                db.write('config_list','name', f"'{files}'")
+                db.write('config_list', 'name', f"'{files}'")
             except:
                 pass
         lst = db.read('config_list WHERE life = 0', 'name')
@@ -32,12 +32,9 @@ def gettok():
         db.update('config_list', 'life = True', f"name = '{free_token}'")
         with open('config/'+free_token, 'rb') as dt:
             data = dt.read()
-        t = cr.import_key(key, data)#.decode('utf8')
+        t = cr.import_key(key, data)
         return t
 
 
-# main driver function
 if __name__ == '__main__':
-    # run() method of Flask class runs the application
-    # on the local development server.
-    app.run(host='0.0.0.0',port=9525)
+    app.run(host='0.0.0.0', port=9525)
--- tester.py
+++ tester.py
@@ -1,15 +1,52 @@
 import requests
-from crypter import Crypterast
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
+from base64 import b64decode
 
-    
-cr = Crypterast()
-cr.generic()
-pub = cr.public_key_final
-link = 'http://127.0.0.1:9525/get'
-# p = requests.get(link).content
-p = requests.post(link, data={'pubkey': cr.public_key_final}).content
-print(p)
-with open('configer', 'w') as f:
-    f.write(cr.decrypt(p).decode())
-    
 
+class Crypterast:
+    def __init__(self) -> None:
+        self.public_key_final = ''
+
+    def generic(self):
+        public_key_start_comment = '-----BEGIN PUBLIC KEY-----'
+        public_key_end_comment = '-----END PUBLIC KEY-----'
+
+        key = RSA.generate(2048)
+        pubkey = key.publickey()
+
+        public_key_pem = pubkey.exportKey(
+            format='PEM', passphrase=None, pkcs=1)
+        self.public_key_final = str(public_key_pem.decode('ascii')).replace('\n', '').replace(
+            public_key_start_comment, '').replace(public_key_end_comment, '').replace(' ', '')
+
+        self._encryptor = PKCS1_OAEP.new(pubkey)
+        self._decryptor = PKCS1_OAEP.new(key)
+
+    def import_key(self, key: str, m):
+        key = b64decode(key.encode())
+        pubKeyObj = RSA.importKey(key)
+        decrypto = PKCS1_OAEP.new(pubKeyObj)
+
+        return decrypto.encrypt(m)
+
+    def encrypt(self, text: bytes):
+        return self._encryptor.encrypt(text)
+
+    def decrypt(self, text: bytes):
+        return self._decryptor.decrypt(text)
+
+
+def download_config():
+    cr = Crypterast()
+    cr.generic()
+    pub = cr.public_key_final
+    get = 'kerc1syuif&x^9!x*fl9kh@8vw05u4wlsf22ch9r'
+    pkey = '8aij&wvq0!1ow^5&x-mdl4sny!gniqxqa-yg*tfy'
+    link = 'http://127.0.0.1:9525/'+get
+    p = requests.post(link, data={pkey: cr.public_key_final}).content
+    with open('configer', 'w') as f:
+        f.write(cr.decrypt(p).decode())
+
+
+download_config()
